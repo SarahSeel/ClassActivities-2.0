7ea5a57c02b832774781f8393eb931c68018f3a4
SarahSeel <sarahizsilly09@yahoo.com>
2023-06-21 21:30:10 -0500

    Week 2

--- Week-2/2.2.2-JSVariablesAndPrimitives.js
+++ Week-2/2.2.2-JSVariablesAndPrimitives.js
@@ -1,31 +1,46 @@
 // create a variable called "name" that references a string
+let name = "Sarah";
 
 // create a variable called "favoriteSong" that references a string
+let favoriteSong = "Bluey";
 
 // create a variable called "wage" that references a number
+let wage = 7;
 
 // create a variable called "age" that references a number
+let age = 32;
 
 // create a variable called "onlyChild" that references a boolean
+let onlyChild = false;
 
 // create a variable called "satisfied" that references a boolean
+let satisfied = true;
 
 // create a variable called "favoriteThing" that references ANY primitive value
 // print the data type of "favoriteThing"
+let favoriteThing = "Unicorns";
+console.log(favoriteThing);
+console.log(typeof favoriteThing);
 
 // You heard a new song that became your favorite.
 // Reassign the variable "favoriteSong"
+favoriteSong = "Bingo";
 
 // You no longer like the type of data stored in "favoriteThing"
 // Reassign "favoriteThing" to a different primitive data type and print its type
+favoriteThing = 2;
 
 // Happy Birthday!
 // Reassign "age" to a new value
+age = 7;
 
 // You got a big raise at work.
 // Reassign "wage" to a new value
+wage = 900;
 
 // Your raise changed your job satisfaction.
 // Assign "satisfied" to true
+satisfied = true;
 
 // use "console.log()" and "+" to print a sentence that includes two of the above variables
+console.log("I am" + age + " and I make " + wage + " dollars per hour");
--- Week-2/2.2.4-JSMorePrimitives.js
+++ Week-2/2.2.4-JSMorePrimitives.js
@@ -1,17 +1,35 @@
 // create five variables and assign them values
 // each variable should reference a different primitive data type
 // REMEMBER: you should never DELIBERATELY assign "undefined" to a variable
+let animal = "Horse";
+let age = 4;
+let like = true;
+let unknown;
+let feeling = null;
 
 // print the type of each variable in this order:
 // string, number, boolean, undefined, null
+console.log(animal);
+console.log(age);
+console.log(like);
+console.log(unknown);
+console.log(feeling);
 
 // create a variable that references a template literal
 // inside the template literal, use two of the above variables
+let today = `If I could have a ${animal} I would want one that is ${age} years old.`;
+console.log(today);
 
 // reassign the value of the variable that references "null"
+feeling = "Happy";
 
 // print the value and its type
+console.log(feeling);
+console.log(typeof feeling);
 
 // print a variable that causes a ReferenceError
+console.log(unicorn);
 
 // alter the previous line to no longer cause a ReferenceError
+let unicorn;
+console.log(unicorn);
