fb5963c03120f5d9c3100854cf365b8f35477220
SarahSeel <sarahizsilly09@yahoo.com>
2023-06-30 13:57:09 -0500

    Review Update

--- Week-3/Week3Review/functionReview.js
+++ Week-3/Week3Review/functionReview.js
@@ -7,8 +7,16 @@
 //    Divide the sum by the number of parameters that were passed in
 //    The function will output the result
 // Hints: Remember how a rest parameter represents arguments and what methods that can allow us to use
+function testAverage(...testScores) {
+  let total = 0;
+  for (let score of testScores) {
+    total += score;
+  }
+  return total / testScores.length;
+}
 
 // Invoke testAverage with the following parameters: 92, 71, 85, 83
+console.log(Math.floor(testAverage(92, 71, 85, 83))); // round down to 82.
 
 // Function Activity 2
 // NOTE: This function really just contains conditionals. The real purpose of Activity 2 is to practice using the returned value of a function as the argument to another function
@@ -21,10 +29,32 @@
 //    if argument is greater than or equal to 80 but less than 90, print "Your grade is B, nice job!" then output false
 //    if argument is greater than or equal to 70, print "Your grade is a C, extra studying required" then output true
 //    if argument is less than 70, print "Uh oh." then output true
+function gradeCheck(grade) {
+  if (grade >= 90) {
+    console.log("Your grade is an A, great job!");
+    return false;
+  }
+  if (grade >= 80) {
+    console.log("Your grade is B, nice job!");
+    return false;
+  }
+  if (grade >= 70) {
+    console.log("Your grade is a C, extra studying required");
+    return true;
+  }
+  if (grade < 70) {
+    console.log("Uh oh.");
+    return true;
+  }
+}
 
 // Invoke gradeCheck and pass in testAverage as an argument; testAverage should have the same parameters as before
 // NOTE: There are a couple of ways to do this. You could invoke testAverage() inside the argument of gradeCheck:
+console.log(gradeCheck(testAverage(92, 71, 85, 83)));
 
 // Or you could create a variable and assign it to the returned value of testAverage()
+let testGrades = testAverage(92, 71, 85, 83);
+console.log(gradeCheck(testGrades));
 
 // Now invoke gradeCheck, but pass in testAverage as an argument that has different parameters
+console.log(gradeCheck(testAverage(90, 83, 77, 84)));
--- Week-3/Week3Review/objectReview.js
+++ Week-3/Week3Review/objectReview.js
@@ -8,20 +8,37 @@ const currentWeather = {
 };
 
 // Print a string that references weather, temp and sky
+console.log(
+  `Today's weather is ${currentWeather.weather}, with the temperature of ${currentWeather.temp} degrees, and the sky conditions ${currentWeather.sky}.`
+);
 
 // Create a variable called weatherDetails and assign it to the string below
+let weatherDetails = "These are the current weather conditions: ";
 
 // Using a for...of loop, iterate through the array stored in "details". Each iteration of the loop should concatenate the value of the item AND a blank space to the end of the "weatherDetails" string.
 // After the for...of loop, print out "weatherDetails"
+for (let detail of currentWeather.details) {
+  weatherDetails += `${detail}`;
+}
+console.log(weatherDetails);
 
 // The exact output for this should be as shown bellow
 // These are the weather details: Moderate Rain 45 Not Severe
 
 // Accessing the details key, Print another string that references each of the properties contained within details
+currentWeather.details[0] = `Moderate Rain, 45, Not Severe`;
 
 // Object Activity 2
 // Create an object called 'movie' that has 3 properties
+const movie = {
+  title: "Disney Pixar Wall-e",
+  genre: "Family",
+  yearReleased: 2009
+};
 // These keys will be "title", "genre", "yearReleased"
 // Each key should have a relevant value
 
 // Iterate over the movie object to print the key AND value of each property
+for (let film in movie) {
+  console.log(`${film} = ${movie[film]}`);
+}
